e28167ab0b57afcf7104c89f5805b8f6bf5b29d2
Dorcas Bamisile <bamisiledorcas@gmail.com>
2023-10-10 12:03:52 +0100

    Fixed trailer

--- src/components/MovieLists.js
+++ src/components/MovieLists.js
@@ -4,13 +4,15 @@ const MovieLists = () => {
   const [movies, setMovies] = useState([]);
   const [trailerVideoIds, setTrailerVideoIds] = useState({});
 
+  const apiKey = "AIzaSyAIKFlFDTiw85gzQOZBGSpdIQuYXlI4XDM";
+
   const fetchTrailer = async (movieTitle) => {
     try {
       const response = await fetch(
-        `https://www.googleapis.com/youtube/v3/search?key=${process.env.YOUTUBE_API_KEY}&part=snippet&q=${movieTitle} trailer&type=video&maxResults=1`
+        `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&part=snippet&q=${movieTitle} trailer&type=video&maxResults=1`
       );
       const data = await response.json();
-      const videoId = data.items[0]?.episode_id?.videoId;
+      const videoId = data.items[0]?.id?.videoId;
       setTrailerVideoIds((prevIds) => ({ ...prevIds, [movieTitle]: videoId }));
     } catch (error) {
       console.log("Error fetching trailer:", error);
@@ -36,7 +38,6 @@ const MovieLists = () => {
             src={`https://www.youtube.com/embed/${
               trailerVideoIds[movie.title]
             }`}
-            frameBorder="0"
             title="trailer"
             className="trailer-iframe"
           ></iframe>
